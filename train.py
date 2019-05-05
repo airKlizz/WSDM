7d8620900bb1fc016086ea74218a1863b56e533b
airklizz <remi.calizzano@gmail.com>
2019-05-05 19:40:44 +0200

    Implemented train script

--- train.py
+++ train.py
@@ -81,9 +81,9 @@ with tf.Graph().as_default():
             indices = np.arange(len(train_X))
             np.random.shuffle(indices)
             print(np.shape(train_X))
-            train_X = train_X[indices[:]]
+            train_X = train_X[list(indices)]
             print(np.shape(train_X))
-            train_y = train_y[indices[:]]
+            train_y = train_y[list(indices)]
             for batch in range(nb_batch_per_epoch):
                 idx_min = batch * batch_size
                 idx_max = min((batch+1) * batch_size, len(train_X)-1)
