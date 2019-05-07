cec3f7cd945e367a9545c00de4533b1dba63fdef
airklizz <remi.calizzano@gmail.com>
2019-05-07 22:54:24 +0200

    Implemented submission script

--- dataset.py
+++ dataset.py
@@ -58,15 +58,15 @@ with open(train_file_path, newline='') as csvfile:
         X_train.append([row[i] for i in [5, 6]])
         y_train.append(row[7])
 
-X_train = X_train[1:1000]
-y_train = y_train[1:1000]
+X_train = X_train[1:]
+y_train = y_train[1:]
 
 with open(test_file_path, newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         X_test.append([row[i] for i in [5, 6]])
 
-X_test = X_test[1:1000]
+X_test = X_test[1:]
 
 '''
 Data preprocessing
