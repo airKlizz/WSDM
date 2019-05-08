fd9170c03b413d6696d96c1d63b6d0e90c590876
airklizz <remi.calizzano@gmail.com>
2019-05-08 15:36:26 +0200

    Implemented submission script

--- train.py
+++ train.py
@@ -9,9 +9,9 @@ import time
 import tensorflow as tf
 import numpy as np
 
-from modelDDD import Model
+from modelSDD import Model
 
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="4"
 
 data_directory = "../Data"
 backup_directory = "../Backup/"
@@ -76,7 +76,7 @@ with tf.Graph().as_default():
 
         timestamp = str(int(time.time()))
         checkpoint_dir = os.path.abspath(backup_directory+timestamp)
-        checkpoint_prefix = os.path.join(checkpoint_dir, "modelDDD")
+        checkpoint_prefix = os.path.join(checkpoint_dir, "modelSDD")
 
         saver = tf.train.Saver(tf.global_variables(), max_to_keep=1)
 
