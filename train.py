026e7505cad4b6d935d443a7f1fe4788679cee1f
airklizz <remi.calizzano@gmail.com>
2019-05-10 18:32:13 +0200

    Implemented submission script

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="1"
 data_directory = "../Data"
 backup_directory = "../Backup/"
 
-dataset_file_path = data_directory+"/train_dataset"
+dataset_file_path = data_directory+"/train_dataset_under_sampling_ration_0.65"
 
 with open(dataset_file_path, 'rb') as f:
     dataset = pickle.load(f)
