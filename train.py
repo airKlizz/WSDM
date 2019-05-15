aabfdcd03034a206ac5bd9d9155de05e7ec7d56d
airklizz <remi.calizzano@gmail.com>
2019-05-15 11:52:19 +0200

    Test

--- train.py
+++ train.py
@@ -9,9 +9,9 @@ import time
 import tensorflow as tf
 import numpy as np
 
-from modelDDD import Model
+from modelSDD import Model
 
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 data_directory = "../Data"
 backup_directory = "../Models/"
@@ -84,7 +84,7 @@ with tf.Graph().as_default():
 
         timestamp = str(int(time.time()))
         checkpoint_dir = os.path.abspath(backup_directory+timestamp)
-        checkpoint_prefix = os.path.join(checkpoint_dir, "1_DDD_original_dataset")
+        checkpoint_prefix = os.path.join(checkpoint_dir, "2_SDD_original_dataset")
 
         saver = tf.train.Saver(tf.global_variables(), max_to_keep=1)
 
