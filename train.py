bc5951deb3f47be2995a43033988978433ce754e
airklizz <remi.calizzano@gmail.com>
2019-05-15 17:54:06 +0200

    Test

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import numpy as np
 
 from modelDDD import Model
 
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 data_directory = "../Data"
 backup_directory = "../Models/"
