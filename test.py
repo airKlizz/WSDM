4d170838de02e210e88c7ad3cfb3c84a88e0ac06
airklizz <remi.calizzano@gmail.com>
2019-05-15 12:39:58 +0200

    Test

--- test.py
+++ test.py
@@ -41,9 +41,9 @@ class_weights = [1/15, 1/5, 1/16]
 #timestamp = '1557654254' #SScv3
 #timestamp = '1557655718' #SScv4
 #timestamp = '1557663468' # SSS dropout
-timestamp = '1557914015' 
+timestamp = '1557914169' 
 
-specifications = 'SDD original dataset'
+specifications = 'SSS original dataset'
 
 
 checkpoint_dir = os.path.abspath(backup_directory+timestamp)
