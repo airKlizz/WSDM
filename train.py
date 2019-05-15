3b987cc03b62fea450ec945d9e826f481414562d
airklizz <remi.calizzano@gmail.com>
2019-05-15 18:28:39 +0200

    Test

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import time
 import tensorflow as tf
 import numpy as np
 
-from modelSSS import Model
+from modelSSS_attention import Model
 
 os.environ["CUDA_VISIBLE_DEVICES"]="3"
 
@@ -84,7 +84,7 @@ with tf.Graph().as_default():
 
         timestamp = str(int(time.time()))
         checkpoint_dir = os.path.abspath(backup_directory+timestamp)
-        checkpoint_prefix = os.path.join(checkpoint_dir, "1_SSS_normalized_batch")
+        checkpoint_prefix = os.path.join(checkpoint_dir, "2_SSS_attention_normalized_batch")
 
         saver = tf.train.Saver(tf.global_variables(), max_to_keep=1)
 
