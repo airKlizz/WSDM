6efccd4114f8f2875f85d5aae72de40a1699fc07
airklizz <remi.calizzano@gmail.com>
2019-05-15 19:41:09 +0200

    Test

--- train.py
+++ train.py
@@ -9,9 +9,9 @@ import time
 import tensorflow as tf
 import numpy as np
 
-from modelSSS import Model
+from modelSSS_multi_LSTM import Model
 
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
+os.environ["CUDA_VISIBLE_DEVICES"]="5"
 
 data_directory = "../Data2"
 backup_directory = "../Backup2/"
@@ -84,7 +84,7 @@ with tf.Graph().as_default():
 
         timestamp = str(int(time.time()))
         checkpoint_dir = os.path.abspath(backup_directory+timestamp)
-        checkpoint_prefix = os.path.join(checkpoint_dir, "2_SSS normalize_batch_size")
+        checkpoint_prefix = os.path.join(checkpoint_dir, "3_SSS_multi_LSTM_normalized_batch")
 
         saver = tf.train.Saver(tf.global_variables(), max_to_keep=1)
 
